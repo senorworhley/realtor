d4ade53d6fae0a67dbcbae929bcf03d802e2e6e4
SenorWorhley <odiyeye@gmail.com>
2025-03-05 20:38:51 +0100

    complete signup ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-<<<<<<< HEAD
 export const signup = async (req, res, next) => {
-=======
-export const signup = async (req, res) => {
->>>>>>> 5fc5a19843273cdd538bc9644c91b4da95d78e7a
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -13,10 +9,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json("User created successfully!");
   } catch (error) {
-<<<<<<< HEAD
     next(error);
-=======
-    res.status(500).json(error.message);
->>>>>>> 5fc5a19843273cdd538bc9644c91b4da95d78e7a
   }
 };
